7a54019a4e55608f8090c3e073d60695b87f054a
Rohit Tatwade <tatwaderohit369@gmail.com>
2024-03-14 13:17:20 +0530

    Bug fix course section

--- src/Components/HomeSections/SectionCourse/CourseSection.jsx
+++ src/Components/HomeSections/SectionCourse/CourseSection.jsx
@@ -12,7 +12,7 @@ const CourseSection = () => {
 
     const [courses, setCourses] = useState([]);
     const [tagsList, setTagsList] = useState([]);
-    const [iframeData, setIframeData] = useState();
+    const [iframeData, setIframeData] = useState([]);
     const [currentTagIndices, setCurrentTagIndices] = useState([]);
 
     const instId = 94;
@@ -20,7 +20,13 @@ const CourseSection = () => {
 
     const getAllCourses = async () => {
         const response = await CourseNetwrok.fetchCourses(instId);
-        setCourses(response.courses);
+        let templist = [];
+        response.courses.forEach((course) => {
+            if (course.active == true) {
+                templist.push(course);
+            }
+        })
+        setCourses(templist);
     };
 
     const getTagsList = async () => {
@@ -31,24 +37,12 @@ const CourseSection = () => {
     const getIframe = async () => {
         const response = await CourseNetwrok.fetchIFrame(iframeId);
         setIframeData(response.iFrame);
-        // Endpoints.mediaBaseUrl = response.mediaUrl
     };
 
     const handleBuyCourse = (item) => {
         const url = `${iframeData?.redirectUri}` + `?iframeId=${iframeId}&courseId=${item?.id}&folderId=${0}&on-click=${true}`
-
-        // const url = `${iframeData?.redirectUri}/buyCourseDetails/${item?.id}/0`
         window.open(url, '_blank', 'noreferrer');
-        // navigate(`/buyCourseDetails/${item.id}`);
     };
-
-    // const handleBuyCourse = (item) => {
-    //     const url = `${iframeData?.redirectUri}` + `?iframeId=${iframeId}&courseId=${item?.id}&folderId=${0}${campaignId ? `&campaignId=${campaignId}` : ''}&on-click=${true}`
-
-    //     // const url = `${iframeData?.redirectUri}/buyCourseDetails/${item?.id}/0`
-    //     window.open(url, '_blank', 'noreferrer');
-    //     // navigate(`/buyCourseDetails/${item.id}`);
-    // }
 
     useEffect(() => {
         getAllCourses();
@@ -60,12 +54,6 @@ const CourseSection = () => {
         if (courses) {
             setCurrentTagIndices(Array(courses.length).fill(0));
         }
-    }, [courses]);
-
-    useEffect(() => {
-        // Filter active courses
-        const activeCourses = courses.filter(course => course.active);
-        setCourses(activeCourses);
     }, [courses]);
 
     const handlePrevClick = (tagIndex) => {
@@ -88,27 +76,7 @@ const CourseSection = () => {
     return (
         <Box sx={{ flexGrow: 1, m: '20px' }}>
             <Grid container spacing={2}>
-                {/* <Grid item xs={12} sm={12} md={12}>
-                    <Typography
-                        textAlign={'center'}
-                        color={'#540000'}
-                        fontSize={'40px'}
-                        fontWeight={'700'}
-                        lineHeight={'60px'}
-                    >
-                        Our Programs
-                    </Typography>
-                </Grid> */}
                 <Grid item xs={12} sm={12} md={12} textAlign={'center'}>
-                    {/* <Box sx={{ flexGrow: 1 }}> */}
-                    {/* <Typography
-                        textAlign={'center'}
-                        fontWeight={'600'}
-                        fontSize={'18px'}
-                        m={'5px'}
-                    >
-                        {tagsList[0]?.tag}
-                    </Typography> */}
                     <Grid container spacing={1}>
                         {tagsList?.length > 0 &&
                             tagsList?.map((tags, i) => {
@@ -122,7 +90,6 @@ const CourseSection = () => {
                                                 </Typography>
                                                 : ""}
                                             <div className='slide-boxes' style={{ width: '100%', overflowX: 'auto', padding: '0.5rem', display: 'flex' }}
-                                            // ref={courseListContainerRef}
                                             >
                                                 <div style={{
                                                     display: 'flex', gap: '20px',
@@ -138,8 +105,6 @@ const CourseSection = () => {
                                                                         onClick={() => handleBuyCourse(item)}
                                                                         key={index} sx={{
                                                                             width: ['90%', '70%'], maxWidth: ['580px', '400px'], height: '100%', borderRadius: '7px',
-                                                                            // background: `${colors?.cardBackground?.default}`,
-                                                                            // color: `${colors?.color?.default}`,
                                                                             boxShadow: '0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)'
                                                                         }} className=''>
                                                                         <div className='relative' style={{ height: '165px' }}>
@@ -155,7 +120,6 @@ const CourseSection = () => {
                                                                             )}
                                                                         </div>
                                                                         <CardContent sx={{ paddingBottom: "20px", padding: '10px' }}>
-                                                                            {/* <Link to={`/courses/${item.id}/0`} style={{ textDecoration: 'none', color: 'inherit' }}> */}
                                                                             <Stack gap={'20px'}>
                                                                                 <Stack direction='row' justifyContent='space-between' alignItems='center'>
                                                                                     <Typography noWrap sx={{ width: '200px', fontWeight: '600', fontSize: '16px' }}>
@@ -164,12 +128,10 @@ const CourseSection = () => {
 
                                                                                 </Stack>
                                                                                 <Typography fontSize={'13px'} noWrap sx={{ width: "300px" }}>
-                                                                                    {/* {parse(first10Words)} */}
                                                                                 </Typography>
                                                                                 <div style={{ width: '100%', display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}>
                                                                                     {item.paid ? (
                                                                                         <Typography variant='body1'
-                                                                                            // color={`${colors?.color?.default}`}
                                                                                             fontWeight={'bold'}>
                                                                                             {item.discount > 0 ? (
                                                                                                 <>
@@ -192,7 +154,6 @@ const CourseSection = () => {
                                                                                     )}
                                                                                 </div>
                                                                             </Stack>
-                                                                            {/* </Link> */}
                                                                         </CardContent>
                                                                         <CardActions sx={{ paddingTop: '0px', display: 'block' }}>
                                                                             <hr style={{ border: '1px solid rgb(128 128 128 / 8%)', width: '95%' }} />
@@ -200,12 +161,10 @@ const CourseSection = () => {
                                                                             <div>
                                                                                 <Button
                                                                                     onClick={() => handleBuyCourse(item)}
-                                                                                    // onClick={isMobile ? handleDrawerOpen : handleDrawerOpen}
                                                                                     fullWidth
                                                                                     sx={{
                                                                                         fontWeight: '700',
                                                                                         textTransform: 'none',
-                                                                                        // background: `${paletteColor.button.default}`,
                                                                                         background: `#F44336FF`,
                                                                                         color: 'white',
                                                                                         '&:hover': {
@@ -249,7 +208,6 @@ const CourseSection = () => {
                                 }
                             })}
                     </Grid>
-                    {/* </Box> */}
                 </Grid>
             </Grid>
         </Box >
